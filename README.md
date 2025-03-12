77af52f58920cb0be28fa8c2550a81106dd1ea23
PerikiyoXD <perikiyoxd@gmail.com>
2025-03-12 14:25:52 +0100

    Add README and implement additional DLL loading functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+# dinput8proxy
+
+Painless, universal DInput8 proxy DLL for games and mods. 
+Allows loading additional DLL files to extend functionality.
+Call it, the universal mod loader.
+
+# Features
+- Load additional DLL files defined in `dinput8proxy.ini`.
--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,10 @@
-#include <windows.h>
+#include <cstdio>
+#include <fstream>
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <format>
+#include <windows.h>
 
 // Forward declare DINPUT8 functions
 typedef HRESULT(WINAPI* DirectInput8Create_t)(HINSTANCE, DWORD, REFIID, LPVOID*, LPUNKNOWN);
@@ -43,13 +48,143 @@ void LoadOriginalDLL()
     }
 }
 
+BOOL ShowMessageBox = true;
+BOOL LoadAdditionalDLLs = true;
+BOOL CloseProcessOnDLLLoadFailure = true;
+BOOL PrintStatusOnLoad = true;
+
+// Additional DLLs
+LPSTR AdditionalDLLs[256];
+int AdditionalDLLCount = 0;
+
+// Load or create the default INI file
+void LoadINI()
+{
+    char path[MAX_PATH];
+    GetModuleFileNameA(0, path, MAX_PATH);
+    strcat_s(path, ".ini");
+
+    ShowMessageBox = GetPrivateProfileIntA("Settings", "ShowMessageBox", 1, path);
+    LoadAdditionalDLLs = GetPrivateProfileIntA("Settings", "LoadAdditionalDLLs", 1, path);
+    CloseProcessOnDLLLoadFailure = GetPrivateProfileIntA("Settings", "CloseProcessOnDLLLoadFailure", 1, path);
+    PrintStatusOnLoad = GetPrivateProfileIntA("Settings", "PrintStatusOnLoad", 1, path);
+}
+
+void CreateDefaultAdditionalDLLsFile()
+{
+    std::ofstream file("AdditionalDLLs.txt");
+    file << "Susano.dll\n";
+    file.close();
+}
+
+void DoLoadAdditionalDLLsFile()
+{
+    if (!LoadAdditionalDLLs) return;
+
+    std::ifstream file("AdditionalDLLs.txt");
+
+    if (!file)
+    {
+        MessageBoxA(0, "Failed to open AdditionalDLLs.txt", "Error", MB_ICONERROR);
+        CreateDefaultAdditionalDLLsFile();
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
+
+        printf("Found additional DLL: %s\n", line.c_str());
+
+        // Add the DLL to the list
+        AdditionalDLLs[AdditionalDLLCount] = (LPSTR)line.c_str();
+        AdditionalDLLCount++;
+    }
+
+    file.close();
+
+    if (AdditionalDLLCount == 0)
+    {
+        MessageBoxA(0, "No additional DLLs found in AdditionalDLLs.txt", "Error", MB_ICONERROR);
+        ExitProcess(1);
+    }
+}
+
+void PrintStatus()
+{
+    printf("DINPUT8 Proxy Console\n");
+    printf("ShowMessageBox: %d\n", ShowMessageBox);
+    printf("LoadAdditionalDLLs: %d\n", LoadAdditionalDLLs);
+    printf("CloseProcessOnDLLLoadFailure: %d\n", CloseProcessOnDLLLoadFailure);
+    printf("AdditionalDLLs: %d\n", AdditionalDLLCount);
+    for (int i = 0; i < AdditionalDLLCount; i++)
+    {
+        printf("  %s\n", AdditionalDLLs[i]);
+    }
+}
+
+// Load additional DLLs from the AdditionalDLLs array
+void DoLoadAdditionalDLLs()
+{
+    if (!LoadAdditionalDLLs) return;
+
+    DoLoadAdditionalDLLsFile();
+
+    for (int i = 0; i < AdditionalDLLCount; i++)
+    {
+        HMODULE hModule = LoadLibraryA(AdditionalDLLs[i]);
+        if (!hModule)
+        {
+            MessageBoxA(0, std::format("Failed to load additional DLL: {0}", AdditionalDLLs[i]).c_str(), "Error", MB_ICONERROR);
+            ExitProcess(1);
+        }
+        else
+        {
+            printf("Loaded additional DLL: %s\n", AdditionalDLLs[i]);
+        }
+    }
+}
+    
+void AllocateConsole()
+{
+    AllocConsole();
+    FILE* fDummy;
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+
+    SetConsoleTitleA("DINPUT8 Proxy Console");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+    SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    // SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), ENABLE_QUICK_EDIT_MODE | ENABLE_EXTENDED_FLAGS | ENABLE_INSERT_MODE | ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT);
+}
+
+
+void DoStuff()
+{
+    LoadOriginalDLL();
+
+    LoadINI();
+
+    if (ShowMessageBox)
+    {
+        MessageBoxA(0, "DINPUT8 Proxy Loaded", "Success", MB_ICONINFORMATION);
+    }
+
+    AllocateConsole();
+
+    PrintStatus();
+
+    DoLoadAdditionalDLLs();
+}
+
 // Proxy functions
 BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
     {
-        MessageBoxA(0, "Hello from DINPUT8.dll", "Info", MB_ICONINFORMATION);
-        LoadOriginalDLL();
+        DoStuff();
     }
     return TRUE;
 }
